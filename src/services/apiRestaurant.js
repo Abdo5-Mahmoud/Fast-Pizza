d534d0a481a49fcaa0f75f96bc5908113066fdc5
Abdo5-Mahmoud <abdofwzy9@gmali.com>
2024-12-04 22:03:49 +0200

    solve the update proplem

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,7 +1,8 @@
+const API_URL_Menu = '/api/menu';
 const API_URL = '/api';
 
 export async function getMenu() {
-  const res = await fetch(`${API_URL}/menu`);
+  const res = await fetch(`${API_URL_Menu}`);
 
   // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
   if (!res.ok) throw Error('Failed getting menu');
